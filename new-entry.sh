45b7dd88b205e5fa8e2b7977b588b692d7aeaf86
kiranjeetkour144@gmail.com <kiranjeet kour>
2025-07-08 13:23:33 +0530

    Added diary entry for 08 July 2025

--- new-entry.sh
+++ new-entry.sh
@@ -22,20 +22,12 @@ date: ${TODAY}
 ---
 
 ## ✅ Tasks Done
-
-1. Had a meeting with boss to learn about testing and related topics.
-2. MOM:
-  - Testing & reviews of 4 July 2025.
-3. State changes in the buttons of Portfolio:
-  - a) "Make it yours" should be shown until no data is added in portfolio. ✔️
-  - b) "Save Changes" & "Discard Changes" should appear when changes are made in the portfolio. ✔️
-  - c) "Preview" button to view current changes in the portfolio.
-4. Published working changes.
-5. Corrected the URL while editing. ✔️
-6. Save every state of the published portfolio.
-7. Routing changes:
-  - 1. If portfolio has data, route to `/dashboard/main`.
-  - 2. Published portfolio routes to the link of the published portfolio. ✔️
+1. Make the instance of the profolio 
+2. Make the versions of the profolio
+3. Make the profolio with the versions
+## 📝 Notes
+- Correct the API first 
+- Make the profolio with the versions
 EOF
 
 echo "✅ Created new diary entry: $FILE_NAME"
